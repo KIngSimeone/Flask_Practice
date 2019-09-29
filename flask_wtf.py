3cca64b92878f5faab0d42176755d05dd7104859
Simeone <simeoneumoh@gmail.com>
2019-09-29 09:52:40 +0100

    Prepopulated the form with data

--- flask_wtf.py
+++ flask_wtf.py
@@ -10,17 +10,25 @@ class LoginForm(FlaskForm):
     username = StringField('Your Username', validators=[InputRequired('A username is required!'), Length(min=4, max=8, message='Must be between 4 and 8 characters')])
     password = PasswordField('password', validators=[InputRequired('Password is required!'), AnyOf(values=['secret', 'password'])])
     age = IntegerField('age', default=24)
-    true = BooleanField('Click Here')
+    true = BooleanField('Click here')
     email = StringField('email', validators=[Email()])
+
+class User:
+    def __init__(self, username, age, email):
+        self.username = username
+        self.age = age
+        self.email = email 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = LoginForm()
+    myuser = User('JohnDoe', 58, 'john@doe.com')
+
+    form = LoginForm(obj=myuser)
 
     if form.validate_on_submit():
         return '<h1>Username: {} Password: {} Age: {} True: {} Email: {}</h1>'.format(form.username.data, form.password.data, form.age.data, form.true.data, form.email.data)
 
-    return render_template('flask_wtf.html', form=form)
+    return render_template('index.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
